2763cf88eb4f8343d3c60270f8878bbd7e56b174
TangRufus <tangrufus@gmail.com>
2025-05-18 04:25:08 +0100

    Remove dummy spec

--- spec/swagger_ui_standalone_spec.rb
+++ spec/swagger_ui_standalone_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe SwaggerUiStandalone do
   it "has a version number" do
     expect(SwaggerUiStandalone::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
