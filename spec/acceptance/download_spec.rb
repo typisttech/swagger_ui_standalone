d1df908e104afd75457fc5bfd0be1c61f792be8e
TangRufus <tangrufus@gmail.com>
2025-05-20 09:29:32 +0100

    Implement `generate` command

--- spec/acceptance/download_spec.rb
+++ spec/acceptance/download_spec.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 RSpec.describe SwaggerUIStandalone::CLI, "download" do
   it "downloads specific ref's swagger-ui dist folder" do
     Dir.mktmpdir do |dir|
+      allow($stdout).to receive(:write)
       described_class.new.invoke(:download, [], output: dir, ref: "v5.21.0")
-      expect(dir).to eq_dir(file_fixture("swagger-ui-5.21.0-dist"))
+      expect(dir).to eq_dir(file_fixture("swagger-ui-5.21.0/dist"))
     end
   end
 end
---
+++ spec/fixtures/golden/swagger-initializer.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+  window.ui = "foo.bar";
+};
